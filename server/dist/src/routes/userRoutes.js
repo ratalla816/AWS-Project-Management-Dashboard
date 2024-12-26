3a36fd520aec52eebc97e7f7e37719488b7698dd
ratalla816 <rob.atalla@robatalla816.com>
2024-12-26 17:02:05 -0600

    pruned

--- server/dist/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const userController_1 = require("../controllers/userController");
-const router = (0, express_1.Router)();
-router.get("/", userController_1.getUsers);
-router.post("/", userController_1.postUser);
-router.get("/:cognitoId", userController_1.getUser);
-exports.default = router;
